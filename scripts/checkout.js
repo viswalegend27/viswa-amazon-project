5931d8d6120cc77083d134c8e3d80b18dae51e96
Viswa v <viswamalar2004@gmail.com>
2026-07-24 14:12:25 +0530

    Generate the HTML for checkout + Created money.js for Currency convertion.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,155 +1,6 @@
 import * as Cart from "../data/cart.js";
 import * as Product from '../data/products.js'
-
-/* <div class="cart-item-container">
-  <div class="delivery-date">
-    Delivery date: Tuesday, June 21
-  </div>
-
-  <div class="cart-item-details-grid">
-    <img class="product-image"
-      src="images/products/athletic-cotton-socks-6-pairs.jpg">
-
-    <div class="cart-item-details">
-      <div class="product-name">
-        Black and Gray Athletic Cotton Socks - 6 Pairs
-      </div>
-      <div class="product-price">
-        $10.90
-      </div>
-      <div class="product-quantity">
-        <span>
-          Quantity: <span class="quantity-label">2</span>
-        </span>
-        <span class="update-quantity-link link-primary">
-          Update
-        </span>
-        <span class="delete-quantity-link link-primary">
-          Delete
-        </span>
-      </div>
-    </div>
-
-    <div class="delivery-options">
-      <div class="delivery-options-title">
-        Choose a delivery option:
-      </div>
-      <div class="delivery-option">
-        <input type="radio" checked
-          class="delivery-option-input"
-          name="delivery-option-1">
-        <div>
-          <div class="delivery-option-date">
-            Tuesday, June 21
-          </div>
-          <div class="delivery-option-price">
-            FREE Shipping
-          </div>
-        </div>
-      </div>
-      <div class="delivery-option">
-        <input type="radio"
-          class="delivery-option-input"
-          name="delivery-option-1">
-        <div>
-          <div class="delivery-option-date">
-            Wednesday, June 15
-          </div>
-          <div class="delivery-option-price">
-            $4.99 - Shipping
-          </div>
-        </div>
-      </div>
-      <div class="delivery-option">
-        <input type="radio"
-          class="delivery-option-input"
-          name="delivery-option-1">
-        <div>
-          <div class="delivery-option-date">
-            Monday, June 13
-          </div>
-          <div class="delivery-option-price">
-            $9.99 - Shipping
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div> 
-
-<div class="cart-item-container">
-  <div class="delivery-date">
-    Delivery date: Wednesday, June 15
-  </div>
-
-  <div class="cart-item-details-grid">
-    <img class="product-image"
-      src="images/products/intermediate-composite-basketball.jpg">
-
-    <div class="cart-item-details">
-      <div class="product-name">
-        Intermediate Size Basketball
-      </div>
-      <div class="product-price">
-        $20.95
-      </div>
-      <div class="product-quantity">
-        <span>
-          Quantity: <span class="quantity-label">1</span>
-        </span>
-        <span class="update-quantity-link link-primary">
-          Update
-        </span>
-        <span class="delete-quantity-link link-primary">
-          Delete
-        </span>
-      </div>
-    </div>
-
-    <div class="delivery-options">
-      <div class="delivery-options-title">
-        Choose a delivery option:
-      </div>
-
-      <div class="delivery-option">
-        <input type="radio" class="delivery-option-input"
-          name="delivery-option-2">
-        <div>
-          <div class="delivery-option-date">
-            Tuesday, June 21
-          </div>
-          <div class="delivery-option-price">
-            FREE Shipping
-          </div>
-        </div>
-      </div>
-      <div class="delivery-option">
-        <input type="radio" checked class="delivery-option-input"
-          name="delivery-option-2">
-        <div>
-          <div class="delivery-option-date">
-            Wednesday, June 15
-          </div>
-          <div class="delivery-option-price">
-            $4.99 - Shipping
-          </div>
-        </div>
-      </div>
-      <div class="delivery-option">
-        <input type="radio" class="delivery-option-input"
-          name="delivery-option-2">
-        <div>
-          <div class="delivery-option-date">
-            Monday, June 13
-          </div>
-          <div class="delivery-option-price">
-            $9.99 - Shipping
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div> */
+import { formatCurrency } from "./utils/money.js";
 
 let checkoutHTML = '';
 
@@ -173,7 +24,7 @@ Cart.cart.forEach((cartItem) => {
         ${product.name}
       </div>
       <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
+        $${formatCurrency(product.priceCents)}
       </div>
       <div class="product-quantity">
         <span>
@@ -193,7 +44,7 @@ Cart.cart.forEach((cartItem) => {
         Choose a delivery option:
       </div>
       <div class="delivery-option">
-        <input type="radio" checked="" class="delivery-option-input" name="delivery-option-1">
+        <input type="radio" class="delivery-option-input" name="delivery-option-${product.id}">
         <div>
           <div class="delivery-option-date">
             Tuesday, June 21
@@ -204,7 +55,7 @@ Cart.cart.forEach((cartItem) => {
         </div>
       </div>
       <div class="delivery-option">
-        <input type="radio" class="delivery-option-input" name="delivery-option-1">
+        <input type="radio" class="delivery-option-input" name="delivery-option-${product.id}">
         <div>
           <div class="delivery-option-date">
             Wednesday, June 15
@@ -215,7 +66,7 @@ Cart.cart.forEach((cartItem) => {
         </div>
       </div>
       <div class="delivery-option">
-        <input type="radio" class="delivery-option-input" name="delivery-option-1">
+        <input type="radio" class="delivery-option-input" name="delivery-option-${product.id}">
         <div>
           <div class="delivery-option-date">
             Monday, June 13
---
+++ scripts/utils/money.js
@@ -0,0 +1,3 @@
+export function formatCurrency(prodPrice){
+  return (prodPrice / 100).toFixed(2);
+}
