25b159332bd7534ec4a0d12bdbfa276b00bbb7fd
Viswa v <viswamalar2004@gmail.com>
2026-09-18 11:22:45 +0530

    created obj-version for cart and included favicon. -- modified all html to include favicon. -- created oop version of cartJS funcs

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import * as orderSummary from "./checkout/orderSummary.js";
 import * as paymentSummary from "./checkout/paymentSummary.js";
+import "./data/cart-oop.js";
 
 orderSummary.renderOrderSummary();
 paymentSummary.paymentSummary();
